6286e0c3a46d161a1055549916f8344e59250b82
RSPFINAL <michaelmardiansyah@students.amikom.ac.id>
2023-05-31 17:49:45 +0700

    slide bar

--- src/Model/Dashboard.js
+++ src/Model/Dashboard.js
@@ -1,6 +1,6 @@
 import Navigation from "../View/navigation";
 import Page1 from "../View/page1";
-import Page2 from "../View/page2";
+
 // import Reservation from "../View/reservation";
 import React from "react";
 import Slider from "../View/slider";
@@ -11,7 +11,7 @@ function Dashboard() {
       <Navigation />
       <Slider />
       <Page1 />
-      <Page2 />
+     
     </body>
   );
 }
