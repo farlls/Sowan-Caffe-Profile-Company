ab5c6f0ddea3a09b10bcadec3bf160c91a30c916
Farrel Shahizidan A <92899505+farlls@users.noreply.github.com>
2023-06-03 15:09:03 +0700

    Add page reservation and config button reservation

--- src/Model/Dashboard.js
+++ src/Model/Dashboard.js
@@ -1,11 +1,12 @@
 import Navigation from "../View/navigation";
-// import Reservation from "../View/reservation";
+import Reservation from "../View/reservation";
 import React from "react";
 
 function Dashboard() {
   return (
     <body>
       <Navigation />
+      {/* <Reservation /> */}
     </body>
   );
 }
--- src/View/navigation.jsx
+++ src/View/navigation.jsx
@@ -1,45 +1,38 @@
-import '../css/Navigation.css';
-import logo from '../assets/img/Sowan Caffe Logo.svg';
-import React from 'react';
-import BtnMenu from '../Controller/NavbarBtn.jsx';
-// import BtnReservation from '../Controller/ReservationBtn.jsx';
-// import Reservation from './reservation.jsx';
+import "../css/Navigation.css";
+import "../css/Reservation.css";
+import logo from "../assets/img/Sowan Caffe Logo.svg";
+import React from "react";
+import BtnMenu from "../Controller/NavbarBtn.jsx";
+import ReservationBtn from '../Controller/ReservationBtn';
 function Navigation() {
-    
   return (
     <nav className="navigation">
-        <div className="container-logo">
-        <img className="logo" alt="Logo Sowan Caffe"  src={logo}></img>
-        </div>
-        <div className="container-navigation-menu">
+      <div className="container-logo">
+        <img className="logo" alt="Logo Sowan Caffe" src={logo}></img>
+      </div>
+      <div className="container-navigation-menu">
         <ul className="navigation-menu">
-                <li>
-                    <a className="active" href="/home">
-                    HOME
-                    </a>
-                </li>
-                <li>
-                    <a href="/about">
-                    ABOUT US
-                    </a>
-                </li>
-                <li>
-                    <a href="/menu">
-                    MENU
-                    </a>
-                </li>
-                <li>
-                    <a href="/contactus">
-                    CONTACT US
-                    </a> 
-                </li>
-            </ul>
-        </div>
-            <div className="main">
-            <button className="reservation-btn"><a  href="/reservation">RESERVATION</a></button>
-            <BtnMenu />
-            </div>
-        </nav>
+          <li>
+            <a className="active" href="/home">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a href="/about">ABOUT US</a>
+          </li>
+          <li>
+            <a href="/menu">MENU</a>
+          </li>
+          <li>
+            <a href="/contactus">CONTACT US</a>
+          </li>
+        </ul>
+      </div>
+      <div className="main">
+        <ReservationBtn />
+        <BtnMenu />
+      </div>
+    </nav>
   );
 }
 
