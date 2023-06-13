4a3c3319ba73d63ea0ddfa2a7caafe20ded54516
nadhiraaurell <nadhiraurelll@gmail.com>
2023-06-13 17:35:35 +0700

    About us bagian 2

--- src/Pages/aboutusPage.jsx
+++ src/Pages/aboutusPage.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import AboutUs from "../View/aboutus";
-<<<<<<< HEAD
 import Aboutus2 from "../View/aboutus2";
-=======
->>>>>>> origin/develop
 import Navigation from "../View/navigation";
 function AboutUS(){
     return(
         <div>   
             <Navigation />
             <AboutUs />
-<<<<<<< HEAD
             <Aboutus2 />
-=======
->>>>>>> origin/develop
         </div>
     );
 }
